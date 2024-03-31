912abd271e2d726888c7239960bb45db29b2f783
Jitesh <anjit193023@gmail.com>
2024-03-31 22:46:24 +0530

    Logic for the timer ( Still has overhead of 300ms )

--- Session/Pages/TimerHomePage.xaml.cs
+++ Session/Pages/TimerHomePage.xaml.cs
@@ -1,51 +1,79 @@
-using System.Diagnostics;
-
 namespace Session.Pages;
 
 public partial class TimerHomePage : ContentPage
 {
-    const string Start = "Start";
-    const string Stop = "Stop";
-    readonly string Second = "00:00";
-    readonly string Minute = "00:00:00";
-    readonly string Hours = "00:00:00:00";
-    private bool _running = false;
+    const string Resume = "Resume";
+    const string Pause = "Pause";
+    readonly string InitialTime = "00:00:00";
+    private bool running = false;
+    private readonly IDispatcherTimer _timer;
+    private DateTime _startTime;
+    private TimeSpan _totalElapsedTime;
+    private bool _isPaused;
 
 
     public TimerHomePage()
     {
         InitializeComponent();
+        _timer = Dispatcher.CreateTimer();
+        _timer.Interval = TimeSpan.FromMilliseconds(600);
+        _timer.Tick += (s, e) => UpdateTimer();
     }
 
-    private void ActionButton_Clicked(object sender, EventArgs e)
+    private void StartButton_Clicked(object sender, EventArgs e)
     {
-        ActionButton.Text = ToggleButtonText(ActionButton.Text);
+        StartButton.IsVisible = false;
+        StopButton.IsVisible = true;
+        PauseButton.IsVisible = true;
+        _startTime = DateTime.Now;
+        running = true;
+        _timer.Start();
+    }
 
-        if (PauseButton.IsVisible)
+    private void PauseButton_Clicked(object? sender, EventArgs e)
+    {
+        _isPaused = !_isPaused;
+
+        if (_isPaused)
         {
-            PauseButton.IsVisible = false;
-            TimeLabel.Text = Second;
-            _running = false;
-            //Do Database operations
+            PauseButton.Text = Resume;
+            _timer.Stop();
         }
         else
         {
-            PauseButton.IsVisible = true;
-            _running = true;
-            RenderStopWatch();
+            PauseButton.Text = Pause;
+            _startTime = DateTime.Now;
+            _timer.Start();
         }
+
     }
 
-    private void RenderStopWatch()
+    private void StopButton_Clicked(object sender, EventArgs e)
     {
-        TimeLabel.Text = Minute;
-        var stopwatch = new Stopwatch();
+        _timer.Stop();
+        _totalElapsedTime = new TimeSpan();
+        TimeLabel.Text = InitialTime;
+        running = false;
+        _isPaused = false;
+        PauseButton.IsVisible = false;
+        StopButton.IsVisible = false;
+        StartButton.IsVisible = true;
 
     }
 
-
-    private string ToggleButtonText(string text)
+    private bool UpdateTimer()
     {
-        return string.Equals(Start, text, StringComparison.OrdinalIgnoreCase) ? Stop : Start;
+        if (running && !_isPaused)
+        {
+            DateTime currentTime = DateTime.Now;
+            TimeSpan elapsedTime = currentTime - _startTime;
+            _totalElapsedTime += elapsedTime;
+            _startTime = currentTime;
+            TimeLabel.Text = String.Format("{0:00}:{1:00}.{2:00}",
+                                            _totalElapsedTime.Minutes, _totalElapsedTime.Seconds, _totalElapsedTime.Milliseconds / 10);
+        }
+        return true;
     }
+
+
 }
